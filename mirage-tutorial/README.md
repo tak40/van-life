a4e4944f6c14cc04f671b11efaad138ae0a10c40
Tak <takeshiseodev@gmail.com>
2023-08-15 14:41:02 -0400

    deleted mirage tutorial file

--- mirage-tutorial/README.md
+++
@@ -1,7 +0,0 @@
-# Mirage Tutorial
-
-This is the Reminders app for the official Mirage tutorial.
-
-Check out [miragejs.com/tutorial](https://miragejs.com/tutorial) to follow along. 
-
-Happy mocking!
--- mirage-tutorial/src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-/*
-  I want a Boolean param that serializes to ?open when true and nothing when false
-  use-query-params serializes "null" as ?name and "undefined" as nothing
-*/
-export const BooleanParam = {
-  encode: (bool) => (bool ? null : undefined),
-  decode: (bool) => bool === null,
-};
