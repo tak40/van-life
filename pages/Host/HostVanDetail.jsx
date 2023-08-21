6b792f73871c2978362f9e60d657c5218a91bb40
Tak <takeshiseodev@gmail.com>
2023-08-21 14:23:40 -0400

    Implement HostVanDetail component and fetch data by ID

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,5 +1,35 @@
 import React from "react"
+import { Link, useParams } from "react-router-dom"
 
 export default function HostVanDetail() {
-    return <h1>Host Van Detail Page</h1>
+    const [vans, setVans] = React.useState([])
+    const params = useParams()
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [params.id])
+
+    const vanElements = vans.map(van => (
+        <section key={van.id}>
+            <div>
+                <img src={van.imageUrl} alt={van.name} />
+            </div>
+            <div>
+                <p>{van.type}</p>
+                <h3>{van.name}</h3>
+                <p>{van.price}<span>day</span></p>
+            </div>
+        </section>
+    ))
+
+    return (
+        <div>
+            {vanElements}
+            <Link>Details</Link>
+            <Link>Pricing</Link>
+            <Link>Photos</Link>
+        </div>
+    )
 }
