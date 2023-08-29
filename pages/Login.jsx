fb0d2e11af6ceeed6b9f6f9643ba023f7b38fc50
Tak <takeshiseodev@gmail.com>
2023-08-28 20:50:24 -0400

    Implement status and error state in Login component

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,13 +1,28 @@
 import React from "react"
 import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+
     const location = useLocation()
     
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+               console.log(data)
+               setError(null) 
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -22,9 +37,12 @@ export default function Login() {
         <div className="login-container">
             {
                 location.state?.message &&
-                <h3 className="login-first">{location.state.message}</h3>
+                <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
+            {
+                error?.message  && 
+                <p className="login-error">{error.message}</p>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -40,7 +58,14 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button 
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting" 
+                        ? "Logging in..." 
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
